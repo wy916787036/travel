5d661fdfbe6b50bba9191ad12dbc21ed9f5e7a3c
wy916787036 <916787036@qq.com>
2018-05-19 15:21:04 +0800

    增加滑动字母表选择城市功能

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list :cities='cities' :hot='hotCities'></city-list>
-    <city-alphabet :cities='cities'></city-alphabet>
+    <city-list :cities='cities' :hot='hotCities' :letter='letter'></city-list>
+    <city-alphabet :cities='cities' @change='handleLetterChange'></city-alphabet>
   </div>
 </template>
 
@@ -24,7 +24,8 @@ export default {
   data () {
     return {
       cities: {},
-      hotCities: []
+      hotCities: [],
+      letter: ''
     }
   },
   methods: {
@@ -32,13 +33,15 @@ export default {
       axios.get('/api/city.json').then(this.handleGetCityInfoSucc)
     },
     handleGetCityInfoSucc (res) {
-      console.log(res)
       res = res.data
       if (res.ret && res.data) {
         const data = res.data
         this.cities = data.cities
         this.hotCities = data.hotCities
       }
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
     }
   },
   mounted () {
